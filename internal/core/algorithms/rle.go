4ba5d62cd33fcf38c9e8e8134f03f250fbdcf4f9
Chi Zu <chizu7993@gmail.com>
2025-12-11 12:39:16 +0000

    Started on RLE

--- internal/core/algorithms/rle.go
+++ internal/core/algorithms/rle.go
@@ -1,5 +1,12 @@
 package algorithms;
 
-func rle(data string) (string, error) {
+import "strings"
 
+// Encrypts data using the RLE compression method.
+// RLE scans the data and replaces repeating consecutive characters with two characters, them being a binary character that has the hexadecimal value of how many of those characters have repeated, followed by a single one of that character that has been repeated.
+// Example: "aaaaaaaaaabbbbbbbbb" -> "(NEWLINE)a(TAB)b", where "a" repeats ten times and "b" repeats nine times, represented by a newline and a tab.
+func rle(data string) (string, error) {
+  if len(data) == 0 { return "", nil } // Handle empty data
+
+  
 }
