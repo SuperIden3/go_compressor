57cd4fceaeb225638a8ffd83a608cbf01460d3ba
samazama892 <ig37225@sersd.org>
2025-12-23 14:55:39 +0000

    Added a few more tests for `rle.go` in `rle_test.go`.

--- internal/core/algorithms/rle_test.go
+++ internal/core/algorithms/rle_test.go
@@ -32,6 +32,21 @@ func TestRle(t *testing.T) {
 			input:    "AAAAAAAAAA", // 10 A's
 			expected: []byte{10, 'A'},
 		},
+		{
+			name:     "Mixed characters",
+			input:    "AABBBCCCCDDDDDE",
+			expected: []byte{2, 'A', 3, 'B', 4, 'C', 5, 'D', 1, 'E'},
+		},
+		{
+			name:     "No repeated characters",
+			input:    "ABCDEFG",
+			expected: []byte{1, 'A', 1, 'B', 1, 'C', 1, 'D', 1, 'E', 1, 'F', 1, 'G'},
+		},
+		{
+			name:     "Overflow",
+			input:    string(bytes.Repeat([]byte{'A'}, 300)), // 300 A's
+			expected: []byte{255, 'A', 45, 'A'}, // 255 + 45
+		},
 	}
 
 	// 2. Run the tests
